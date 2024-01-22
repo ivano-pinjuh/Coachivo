faec00520f1754b8a92459f0744d6d36a4ccc1bb
Ivano Pinjuh <pinjuh.ivano123@gmail.com>
2024-01-22 12:16:12 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='font-bold'>init!</div>
+    </>
+  )
+}
+
+export default App
