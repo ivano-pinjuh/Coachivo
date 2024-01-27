6a54cc03013ff3e0b3a5fff7d24929ef573355c0
ivanoou <pinjuh.ivano123@gmail.com>
2024-01-27 18:31:37 +0100

    some navbar redesign

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,7 @@
 import { IoIosMenu } from 'react-icons/io'
+import { IoIosSearch } from "react-icons/io"
+import { MdOutlineShoppingCart } from 'react-icons/md' 
+
 import { useState } from 'react'
 
 const Navbar = () => {
@@ -16,16 +19,30 @@ const Navbar = () => {
   }
 
   return (
-    <div className='flex z-40 bg-white items-center justify-between relative h-[60px] w-full shadow-md pl-[20px] pr-[20px] overflow-x-clip border-b-2 border-black'>
-      <div className='flex items-center h-[100%]'>  
-        <img className='h-[80%]' src="/coachivo.png" alt="" />
-        <a className='font-bold text-[16px]' href="">Coachivo</a>
+    <div className='flex justify-between items-center w-full h-[72px] shadow-md overflow-clip'>
+      <img className='h-[48px] -translate-y-2' src="/logo.svg" alt="Logo" />
+      
+      <a className='text-lg font-semibold font-poppins px-8 pr-12' href="">Courses</a>
+
+      <div className='flex items-center justify-between rounded h-[42px] border-black border-[0.5px] w-80'>
+        <input className='h-10 px-4 rounded-l font-roboto-slab text-xs' type="search" placeholder='Search for anything' />
+        <div className='flex justify-center items-center rounded-r h-[42px] w-[42px] bg-azure'>
+          <IoIosSearch className='text-milk text-2xl' />
+        </div>
       </div>
-      <IoIosMenu onClick={showMenuHandler} className='lg:hidden text-3xl' />
 
+      <div className='flex w-fit gap-8 px-6'>
+        <a className='text-lg font-semibold font-poppins' href="">Become a coach</a>
+        <a className='text-lg font-semibold font-poppins' href="">Pricing</a>
+      </div>
 
-      <div id='sidebar-menu' className='flex h-40 w-full border-b-2 border-black bg-pink-300 md:hidden translate-x-[100%] z-50 absolute top-0 right-0 transition-all'>
-        <h3 onClick={showMenuHandler}>close</h3>
+      <MdOutlineShoppingCart className='text-4xl mx-6' />
+
+      <div className='bg-grayish h-full w-fit px-12 skew-x-[-18deg] translate-x-10'>
+        <div className='flex items-center gap-6 h-full skew-x-[18deg] mr-8'>
+          <a className='text-brilliant-azure font-roboto-slab' href="">Log in</a>
+          <a className='rounded-[4px] bg-azure text-milk font-roboto-slab p-2 px-5 text-sm' href="">Join for Free</a>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      'roboto-slab': ['Roboto Slab', 'serif']
+    },
+    colors: {
+      'milk': '#FAFBF6',
+      'isabelline': '#F1F1EC',
+      'grayish': '#E8E8E8',
+      'azure': '#0C86FE',
+      'brilliant-azure': '#3C9DFC',
+      'midnight-green': '#0F3E59',
+
+    },
     extend: {},
   },
   plugins: [],
