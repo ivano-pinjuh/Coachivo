c2f01b79a846a415f042a0e630e4daf41ee6d2a6
ivanoou <pinjuh.ivano123@gmail.com>
2024-02-04 22:51:39 +0100

    added the ability to switch tabs in courses section

--- src/components/sections/NewCourses.jsx
+++ src/components/sections/NewCourses.jsx
@@ -1,9 +1,23 @@
-import { useContext } from "react"
+import { useContext, useRef, useState } from "react"
 import CourseCard from "../UI/CourseCard"
 import { CoursesContext } from "../../supabase/courses-context"
 
 const NewCourses = () => {
   const { coursesData, setCoursesData } = useContext(CoursesContext)
+
+  const [currentArea, setCurrentArea] = useState("Defense")
+
+  const tabRef = useRef()
+
+  const changeTabHandler = (event) => {
+    tabRef.current.classList.add("opacity-50")
+    tabRef.current.classList.remove("font-semibold")
+
+    setCurrentArea(event.target.dataset.area)
+    event.target.classList.remove("opacity-50")
+    event.target.classList.add("font-semibold")
+    tabRef.current = event.target
+  }
 
   return (
     <div className="w-full bg-isabelline pb-[10vh] pt-[10vh] md:pt-[12vh] px-[10vw]">
@@ -12,16 +26,16 @@ const NewCourses = () => {
       </h1>
 
       <div className="">
-        <div className="hidden md:flex w-full mt-[6vh]">
-          <h3 className="font-roboto-slab text-lg px-10 pb-2 border-b-2 cursor-pointer font-semibold">
+        <div className="hidden md:flex w-full mt-[5vh]">
+          <h3 data-area='Defense' ref={tabRef} className="font-roboto-slab text-lg px-10 py-2 border-b-2 cursor-pointer font-semibold hover:bg-opacity-20 hover:bg-azure transition-all" onClick={changeTabHandler}>
             Build-up Play
           </h3>
 
-          <h3 className="font-roboto-slab text-lg px-10 pb-2 border-b-2 opacity-50 cursor-pointer font-semibold">
+          <h3 data-area='Midfield' className="font-roboto-slab text-lg px-10 py-2 border-b-2 opacity-50 cursor-pointer hover:bg-opacity-20 hover:bg-azure transition-all" onClick={changeTabHandler}>
             Midfield Excellence
           </h3>
 
-          <h3 className="font-roboto-slab text-lg px-10 pb-2 border-b-2 opacity-50 cursor-pointer font-semibold">
+          <h3 data-area='Attack' className="font-roboto-slab text-lg px-10 py-2 border-b-2 opacity-50 cursor-pointer hover:bg-opacity-20 hover:bg-azure transition-all" onClick={changeTabHandler}>
             Attacking Prowess
           </h3>
             
@@ -29,10 +43,9 @@ const NewCourses = () => {
         </div>
 
         <div className="flex mt-10 w-full flex-wrap gap-y-4 justify-between">
-          <CourseCard course={coursesData[0]} />
-          <CourseCard course={coursesData[1]} />
-          <CourseCard course={coursesData[2]} />
-          <CourseCard course={coursesData[3]} />
+          {coursesData.filter(course => course.area_of_field == currentArea).slice(0, 4).map(course => (
+            <CourseCard course={course} />
+          ))}
         </div>
       </div>
 
